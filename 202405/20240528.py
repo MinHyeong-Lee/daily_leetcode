e334144c9306e34ae7e104498be3f0617574da82
MinHyeong-Lee <usupool94@gmail.com>
2024-05-28 10:22:13 +0900

    solution for [20240528] 1208. Get Equal Substr...

--- 202405/20240528.py
+++ 202405/20240528.py
@@ -17,6 +17,22 @@ class Solution:
 
     """
 
-    def equal_substring(self, s: str, t: str, max_cost: int) -> int: ...
+    def equal_substring(self, s: str, t: str, max_cost: int) -> int:
+        n = len(s)
+
+        start = 0
+        current_cost = 0
+        max_length = 0
+
+        for end in range(n):
+            current_cost += abs(ord(s[end]) - ord(t[end]))
+
+            while current_cost > max_cost:
+                current_cost -= abs(ord(s[start]) - ord(t[start]))
+                start += 1
+
+            max_length = max(max_length, end - start + 1)
+
+        return max_length
 
     equalSubstring = equal_substring
