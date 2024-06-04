af9f90448458df67df9a121a186e7ccc93480d50
MinHyeong Lee <usupool94@gmail.com>
2024-06-04 00:29:51 +0000

    Boilerplate for 20240604
    https://leetcode.com/problems/longest-palindrome/

--- 202405/20240531.py
+++ 202405/20240531.py
@@ -15,25 +15,26 @@ class Solution:
     두 종류의 elements는 오직 1개씩만 들어있고
     나머지 모든 elements는 2개씩 들어있다.
     1개씩만 들어있는 숫자를 찾아라. 리턴할때 sorting은 상관없음.
-    
+
     중복값 찾을때 hash map(dict)를 사용하는게 좋음
     """
 
     def single_number(self, nums: list[int]) -> list[int]:
         frequency = {}
-        
+
         for num in nums:
             if num in frequency:
                 frequency[num] += 1
             else:
                 frequency[num] = 1
-                
+
         unique_numbers = [num for num in frequency if frequency[num] == 1]
-        
+
         print(unique_numbers)
         return unique_numbers
 
     singleNumber = single_number
+
 
 temp = Solution()
 temp.singleNumber([1, 2, 1, 3, 2, 5])
