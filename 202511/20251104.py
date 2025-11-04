690a6dfce98c4b1e26cbe3ab73f509cc3c62818d
MinHyeong Lee <usupool94@gmail.com>
2025-11-04 00:42:33 +0000

    Boilerplate for 20251104
    https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i/

---
+++ 202511/20251104.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/find-x-sum-of-all-k-long-subarrays-i/
+
+
+class Solution:
+    """3318. Find X-Sum of All K-Long Subarrays I
+
+    You are given an array `nums` of `n` integers and two integers `k` and `x`.
+
+    The **x-sum** of an array is calculated by the following procedure:
+
+    * Count the occurrences of all elements in the array.
+
+    * Keep only the occurrences of the top `x` most frequent elements. If two elements
+    have the same number of occurrences, the element with the **bigger** value is
+    considered more frequent.
+
+    * Calculate the sum of the resulting array.
+
+    **Note** that if an array has less than `x` distinct elements, its **x-sum** is the
+    sum of the array.
+
+    Return an integer array `answer` of length `n - k + 1` where `answer[i]` is the
+    **x-sum** of the subarray `nums[i..i + k - 1]`."""
+
+    def find_x_sum(self, nums: list[int], k: int, x: int) -> list[int]: ...
+
+    findXSum = find_x_sum
