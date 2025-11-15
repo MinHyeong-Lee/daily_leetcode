47b9d4331c4e97bc6a16c7b6ad5b1f58ceb17262
MinHyeong Lee <usupool94@gmail.com>
2025-11-15 00:42:51 +0000

    Boilerplate for 20251115
    https://leetcode.com/problems/count-the-number-of-substrings-with-dominant-ones/

---
+++ 202511/20251115.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/count-the-number-of-substrings-with-dominant-ones/
+
+
+class Solution:
+    """3234. Count the Number of Substrings With Dominant Ones
+
+    You are given a binary string `s`.
+
+    Return the number of substrings with **dominant** ones.
+
+    A string has **dominant** ones if the number of ones in the string is **greater than
+    or equal to** the **square** of the number of zeros in the string."""
+
+    def number_of_substrings(self, s: str) -> int: ...
+
+    numberOfSubstrings = number_of_substrings
