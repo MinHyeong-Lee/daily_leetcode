b8ee884b2be083282f5cd4f1c4b6a91c58fe6c62
MinHyeong Lee <usupool94@gmail.com>
2025-11-16 00:47:24 +0000

    Boilerplate for 20251116
    https://leetcode.com/problems/number-of-substrings-with-only-1s/

---
+++ 202511/20251116.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/number-of-substrings-with-only-1s/
+
+
+class Solution:
+    """1513. Number of Substrings With Only 1s
+
+    Given a binary string `s`, return *the number of substrings with all characters*
+    `1`*'s*. Since the answer may be too large, return it modulo `109 + 7`."""
+
+    def num_sub(self, s: str) -> int: ...
+
+    numSub = num_sub
