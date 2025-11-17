054346575b625c8edba83cfe98b1ead910463caa
MinHyeong Lee <usupool94@gmail.com>
2025-11-17 00:45:08 +0000

    Boilerplate for 20251117
    https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/

---
+++ 202511/20251117.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/
+
+
+class Solution:
+    """1437. Check If All 1's Are at Least Length K Places Away
+
+    Given an binary array `nums` and an integer `k`, return `true` *if all* `1`*'s are
+    at least* `k` *places away from each other, otherwise return* `false`."""
+
+    def k_length_apart(self, nums: list[int], k: int) -> bool: ...
+
+    kLengthApart = k_length_apart
