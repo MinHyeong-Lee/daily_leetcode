a81fa92ed84e4badea14ed9e24e23c5ebab8a1ca
MinHyeong Lee <usupool94@gmail.com>
2025-11-25 00:42:32 +0000

    Boilerplate for 20251125
    https://leetcode.com/problems/smallest-integer-divisible-by-k/

---
+++ 202511/20251125.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/smallest-integer-divisible-by-k/
+
+
+class Solution:
+    """1015. Smallest Integer Divisible by K
+
+    Given a positive integer `k`, you need to find the **length** of the **smallest**
+    positive integer `n` such that `n` is divisible by `k`, and `n` only contains the
+    digit `1`.
+
+    Return *the **length** of* `n`. If there is no such `n`, return -1.
+
+    **Note:** `n` may not fit in a 64-bit signed integer."""
+
+    def smallest_repunit_div_by_k(self, k: int) -> int: ...
+
+    smallestRepunitDivByK = smallest_repunit_div_by_k
--- 202511/README.md
+++ 202511/README.md
@@ -169,7 +169,9 @@
    </a>
   </td>
   <td class="tue">
-   25
+   <a href="20251125.py">
+    25
+   </a>
   </td>
   <td class="wed">
    26
