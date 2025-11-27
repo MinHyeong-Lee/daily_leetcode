757b1a84967244edb280a10a4137164c3c407b8c
MinHyeong Lee <usupool94@gmail.com>
2025-11-27 00:43:07 +0000

    Boilerplate for 20251127
    https://leetcode.com/problems/maximum-subarray-sum-with-length-divisible-by-k/

---
+++ 202511/20251127.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/maximum-subarray-sum-with-length-divisible-by-k/
+
+
+class Solution:
+    """3381. Maximum Subarray Sum With Length Divisible by K
+
+    You are given an array of integers `nums` and an integer `k`.
+
+    Return the **maximum** sum of a subarray of `nums`, such that the size of the
+    subarray is **divisible** by `k`."""
+
+    def max_subarray_sum(self, nums: list[int], k: int) -> int: ...
+
+    maxSubarraySum = max_subarray_sum
