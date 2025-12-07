dafadd9714734adbf55a4f55ce27a413815e80f2
MinHyeong Lee <usupool94@gmail.com>
2025-12-07 00:51:10 +0000

    Boilerplate for 20251207
    https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/

---
+++ 202512/20251207.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/
+
+
+class Solution:
+    """1523. Count Odd Numbers in an Interval Range
+
+    Given two non-negative integers `low` and `high`. Return the *count of odd numbers
+    between* `low` *and* `high`*(inclusive)*."""
+
+    def count_odds(self, low: int, high: int) -> int: ...
+
+    countOdds = count_odds
