c625d6595be7da02046bbd715847b72d123b5b46
MinHyeong Lee <usupool94@gmail.com>
2025-12-08 00:45:22 +0000

    Boilerplate for 20251208
    https://leetcode.com/problems/count-square-sum-triples/

---
+++ 202512/20251208.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/count-square-sum-triples/
+
+
+class Solution:
+    """1925. Count Square Sum Triples
+
+    A **square triple** `(a,b,c)` is a triple where `a`, `b`, and `c` are **integers**
+    and `a2 + b2 = c2`.
+
+    Given an integer `n`, return *the number of **square triples** such that* `1 <= a,
+    b, c <= n`."""
+
+    def count_triples(self, n: int) -> int: ...
+
+    countTriples = count_triples
