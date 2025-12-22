83659eb064df851c6e76955786e6b91f897774f2
MinHyeong Lee <usupool94@gmail.com>
2025-12-22 00:50:11 +0000

    Boilerplate for 20251222
    https://leetcode.com/problems/delete-columns-to-make-sorted-iii/

---
+++ 202512/20251222.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/delete-columns-to-make-sorted-iii/
+
+
+class Solution:
+    """960. Delete Columns to Make Sorted III
+
+    You are given an array of `n` strings `strs`, all of the same length.
+
+    We may choose any deletion indices, and we delete all the characters in those
+    indices for each string.
+
+    For example, if we have `strs = ["abcdef","uvwxyz"]` and deletion indices `{0, 2,
+    3}`, then the final array after deletions is `["bef", "vyz"]`.
+
+    Suppose we chose a set of deletion indices `answer` such that after deletions, the
+    final array has **every string (row) in lexicographic** order. (i.e., `(strs[0][0]
+    <= strs[0][1] <= ... <= strs[0][strs[0].length - 1])`, and `(strs[1][0] <=
+    strs[1][1] <= ... <= strs[1][strs[1].length - 1])`, and so on). Return *the minimum
+    possible value of* `answer.length`."""
+
+    def min_deletion_size(self, strs: list[str]) -> int: ...
+
+    minDeletionSize = min_deletion_size
