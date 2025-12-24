403389dbee538ccd2abb67c03fc18b163bb3093d
MinHyeong Lee <usupool94@gmail.com>
2025-12-24 00:46:13 +0000

    Boilerplate for 20251224
    https://leetcode.com/problems/apple-redistribution-into-boxes/

---
+++ 202512/20251224.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/apple-redistribution-into-boxes/
+
+
+class Solution:
+    """3074. Apple Redistribution into Boxes
+
+    You are given an array `apple` of size `n` and an array `capacity` of size `m`.
+
+    There are `n` packs where the `ith` pack contains `apple[i]` apples. There are `m`
+    boxes as well, and the `ith` box has a capacity of `capacity[i]` apples.
+
+    Return *the **minimum** number of boxes you need to select to redistribute these*
+    `n` *packs of apples into boxes*.
+
+    **Note** that, apples from the same pack can be distributed into different boxes."""
+
+    def minimum_boxes(self, apple: list[int], capacity: list[int]) -> int: ...
+
+    minimumBoxes = minimum_boxes
