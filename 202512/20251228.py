11a264c8dd847997cf4e782c4bc41973f8f68a47
MinHyeong Lee <usupool94@gmail.com>
2025-12-28 00:53:55 +0000

    Boilerplate for 20251228
    https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/

---
+++ 202512/20251228.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+
+
+class Solution:
+    """1351. Count Negative Numbers in a Sorted Matrix
+
+    Given a `m x n` matrix `grid` which is sorted in non-increasing order both row-wise
+    and column-wise, return *the number of **negative** numbers in* `grid`."""
+
+    def count_negatives(self, grid: list[list[int]]) -> int: ...
+
+    countNegatives = count_negatives
