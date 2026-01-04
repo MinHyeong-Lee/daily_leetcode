5a421ec9b55c4390662e30792a287736abe104f7
MinHyeong Lee <usupool94@gmail.com>
2026-01-04 00:54:22 +0000

    Boilerplate for 20260104
    https://leetcode.com/problems/four-divisors/

---
+++ 202601/20260104.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/four-divisors/
+
+
+class Solution:
+    """1390. Four Divisors
+
+    Given an integer array `nums`, return *the sum of divisors of the integers in that
+    array that have exactly four divisors*. If there is no such integer in the array,
+    return `0`."""
+
+    def sum_four_divisors(self, nums: list[int]) -> int: ...
+
+    sumFourDivisors = sum_four_divisors
