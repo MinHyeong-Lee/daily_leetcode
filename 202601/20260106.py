4fafd7676bc740ed433ff8d3f777cb31aec693ea
MinHyeong Lee <usupool94@gmail.com>
2026-01-06 00:48:25 +0000

    Boilerplate for 20260106
    https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/

---
+++ 202601/20260106.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+
+
+class Solution:
+    """1161. Maximum Level Sum of a Binary Tree
+
+    Given the `root` of a binary tree, the level of its root is `1`, the level of its
+    children is `2`, and so on.
+
+    Return the **smallest** level `x` such that the sum of all the values of nodes at
+    level `x` is **maximal**."""
+
+    def max_level_sum(self, root: Optional[TreeNode]) -> int: ...
+
+    maxLevelSum = max_level_sum
