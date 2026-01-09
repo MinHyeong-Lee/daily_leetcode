2d2485b3eabd57a0cc09e76506da2e0590e871ad
MinHyeong Lee <usupool94@gmail.com>
2026-01-09 00:48:31 +0000

    Boilerplate for 20260109
    https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/

---
+++ 202601/20260109.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+
+
+class Solution:
+    """865. Smallest Subtree with all the Deepest Nodes
+
+    Given the `root` of a binary tree, the depth of each node is **the shortest distance
+    to the root**.
+
+    Return *the smallest subtree* such that it contains **all the deepest nodes** in the
+    original tree.
+
+    A node is called **the deepest** if it has the largest depth possible among any node
+    in the entire tree.
+
+    The **subtree** of a node is a tree consisting of that node, plus the set of all
+    descendants of that node."""
+
+    def subtree_with_all_deepest(
+        self, root: Optional[TreeNode]
+    ) -> Optional[TreeNode]: ...
+
+    subtreeWithAllDeepest = subtree_with_all_deepest
