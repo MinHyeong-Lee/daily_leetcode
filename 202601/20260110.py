94470d3b2fdcf151da18ed0070b120384ead9fe3
MinHyeong Lee <usupool94@gmail.com>
2026-01-10 00:46:48 +0000

    Boilerplate for 20260110
    https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/

---
+++ 202601/20260110.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/minimum-ascii-delete-sum-for-two-strings/
+
+
+class Solution:
+    """712. Minimum ASCII Delete Sum for Two Strings
+
+    Given two strings `s1` and `s2`, return *the lowest **ASCII** sum of deleted
+    characters to make two strings equal*."""
+
+    def minimum_delete_sum(self, s1: str, s2: str) -> int: ...
+
+    minimumDeleteSum = minimum_delete_sum
--- 202601/README.md
+++ 202601/README.md
@@ -84,7 +84,9 @@
    </a>
   </td>
   <td class="sat">
-   10
+   <a href="20260110.py">
+    10
+   </a>
   </td>
  </tr>
  <tr>
