39e17e4d797f664828bef18c17a832ee609422b4
MinHyeong Lee <usupool94@gmail.com>
2026-02-01 01:07:18 +0000

    Boilerplate for 20260201
    https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/

---
+++ 202602/20260201.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/
+
+
+class Solution:
+    """3010. Divide an Array Into Subarrays With Minimum Cost I
+
+    You are given an array of integers `nums` of length `n`.
+
+    The **cost** of an array is the value of its **first** element. For example, the
+    cost of `[1,2,3]` is `1` while the cost of `[3,4,1]` is `3`.
+
+    You need to divide `nums` into `3` **disjoint contiguous** subarrays.
+
+    Return *the **minimum** possible **sum** of the cost of these subarrays*."""
+
+    def minimum_cost(self, nums: list[int]) -> int: ...
+
+    minimumCost = minimum_cost
