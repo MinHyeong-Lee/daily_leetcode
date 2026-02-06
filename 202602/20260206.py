1dd5b2a9a005a68ad418a11b1de0497697948d1e
MinHyeong Lee <usupool94@gmail.com>
2026-02-06 00:56:47 +0000

    Boilerplate for 20260206
    https://leetcode.com/problems/minimum-removals-to-balance-array/

---
+++ 202602/20260206.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/minimum-removals-to-balance-array/
+
+
+class Solution:
+    """3634. Minimum Removals to Balance Array
+
+    You are given an integer array `nums` and an integer `k`.
+
+    An array is considered **balanced** if the value of its **maximum** element is **at
+    most** `k` times the **minimum** element.
+
+    You may remove **any** number of elements from `nums`\u200b\u200b\u200b\u200b\u200b\u200b\u200b without making it
+    **empty**.
+
+    Return the **minimum** number of elements to remove so that the remaining array is
+    balanced.
+
+    **Note:** An array of size 1 is considered balanced as its maximum and minimum are
+    equal, and the condition always holds true."""
+
+    def min_removal(self, nums: list[int], k: int) -> int: ...
+
+    minRemoval = min_removal
