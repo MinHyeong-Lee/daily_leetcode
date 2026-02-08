78843072625fd6762dcbfd7e1753f8a1e99c890f
MinHyeong Lee <usupool94@gmail.com>
2026-02-08 01:16:48 +0000

    Boilerplate for 20260208
    https://leetcode.com/problems/balanced-binary-tree/

---
+++ 202602/20260208.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/balanced-binary-tree/
+
+
+class Solution:
+    """110. Balanced Binary Tree
+
+    Given a binary tree, determine if it is **height-balanced**."""
+
+    def is_balanced(self, root: Optional[TreeNode]) -> bool: ...
+
+    isBalanced = is_balanced
