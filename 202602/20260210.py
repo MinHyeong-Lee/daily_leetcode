68f3f2462182f7adf2393c00bfac01a0ce7e7bad
MinHyeong Lee <usupool94@gmail.com>
2026-02-10 01:11:01 +0000

    Boilerplate for 20260210
    https://leetcode.com/problems/longest-balanced-subarray-i/

---
+++ 202602/20260210.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/longest-balanced-subarray-i/
+
+
+class Solution:
+    """3719. Longest Balanced Subarray I
+
+    You are given an integer array `nums`.
+
+    A **subarray** is called **balanced** if the number of **distinct even** numbers in
+    the subarray is equal to the number of **distinct odd** numbers.
+
+    Return the length of the **longest** balanced subarray."""
+
+    def longest_balanced(self, nums: list[int]) -> int: ...
+
+    longestBalanced = longest_balanced
