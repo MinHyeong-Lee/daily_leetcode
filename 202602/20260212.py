a667803f8d583b35c29a6429ac48f813c9d33af2
MinHyeong Lee <usupool94@gmail.com>
2026-02-12 01:01:32 +0000

    Boilerplate for 20260212
    https://leetcode.com/problems/longest-balanced-substring-i/

---
+++ 202602/20260212.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/longest-balanced-substring-i/
+
+
+class Solution:
+    """3713. Longest Balanced Substring I
+
+    You are given a string `s` consisting of lowercase English letters.
+
+    A **substring** of `s` is called **balanced** if all **distinct** characters in the
+    **substring** appear the **same** number of times.
+
+    Return the **length** of the **longest balanced substring** of `s`."""
+
+    def longest_balanced(self, s: str) -> int: ...
+
+    longestBalanced = longest_balanced
