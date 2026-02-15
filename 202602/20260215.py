9c8b2350e6f944a96a195b70af392cebf1dadf13
MinHyeong Lee <usupool94@gmail.com>
2026-02-15 01:03:36 +0000

    Boilerplate for 20260215
    https://leetcode.com/problems/add-binary/

---
+++ 202602/20260215.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/add-binary/
+
+
+class Solution:
+    """67. Add Binary
+
+    Given two binary strings `a` and `b`, return *their sum as a binary string*."""
+
+    def add_binary(self, a: str, b: str) -> str: ...
+
+    addBinary = add_binary
