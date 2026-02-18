4a0e854d6e8a20b9bc24800dd5521cd18ca94ce7
MinHyeong Lee <usupool94@gmail.com>
2026-02-18 01:02:45 +0000

    Boilerplate for 20260218
    https://leetcode.com/problems/binary-number-with-alternating-bits/

---
+++ 202602/20260218.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/binary-number-with-alternating-bits/
+
+
+class Solution:
+    """693. Binary Number with Alternating Bits
+
+    Given a positive integer, check whether it has alternating bits: namely, if two
+    adjacent bits will always have different values."""
+
+    def has_alternating_bits(self, n: int) -> bool: ...
+
+    hasAlternatingBits = has_alternating_bits
