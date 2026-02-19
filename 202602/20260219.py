6b535d0cf4e5af5b2dec92e964bc67bdb26070ef
MinHyeong Lee <usupool94@gmail.com>
2026-02-19 01:02:04 +0000

    Boilerplate for 20260219
    https://leetcode.com/problems/count-binary-substrings/

---
+++ 202602/20260219.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/count-binary-substrings/
+
+
+class Solution:
+    """696. Count Binary Substrings
+
+    Given a binary string `s`, return the number of non-empty substrings that have the
+    same number of `0`'s and `1`'s, and all the `0`'s and all the `1`'s in these
+    substrings are grouped consecutively.
+
+    Substrings that occur multiple times are counted the number of times they occur."""
+
+    def count_binary_substrings(self, s: str) -> int: ...
+
+    countBinarySubstrings = count_binary_substrings
