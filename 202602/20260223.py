395c06b729d020b303e1e0da4eef4b480a71b51f
MinHyeong Lee <usupool94@gmail.com>
2026-02-23 01:00:57 +0000

    Boilerplate for 20260223
    https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/

---
+++ 202602/20260223.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+
+
+class Solution:
+    """1461. Check If a String Contains All Binary Codes of Size K
+
+    Given a binary string `s` and an integer `k`, return `true` *if every binary code of
+    length* `k` *is a substring of* `s`. Otherwise, return `false`."""
+
+    def has_all_codes(self, s: str, k: int) -> bool: ...
+
+    hasAllCodes = has_all_codes
