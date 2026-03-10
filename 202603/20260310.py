f2e5507bd894a7e006f30e7dc7e819dae3a2ecd3
MinHyeong Lee <usupool94@gmail.com>
2026-03-10 00:54:57 +0000

    Boilerplate for 20260310
    https://leetcode.com/problems/find-all-possible-stable-binary-arrays-ii/

---
+++ 202603/20260310.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/find-all-possible-stable-binary-arrays-ii/
+
+
+class Solution:
+    """3130. Find All Possible Stable Binary Arrays II
+
+    You are given 3 positive integers `zero`, `one`, and `limit`.
+
+    A binary array `arr` is called **stable** if:
+
+    * The number of occurrences of 0 in `arr` is **exactly** `zero`.
+
+    * The number of occurrences of 1 in `arr` is **exactly** `one`.
+
+    * Each subarray of `arr` with a size greater than `limit` must contain **both** 0
+    and 1.
+
+    Return the *total* number of **stable** binary arrays.
+
+    Since the answer may be very large, return it **modulo** `109 + 7`."""
+
+    def number_of_stable_arrays(self, zero: int, one: int, limit: int) -> int: ...
+
+    numberOfStableArrays = number_of_stable_arrays
