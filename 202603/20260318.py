540154f67fc3233b29df6e5ff9d235ae9fa68c9c
MinHyeong Lee <usupool94@gmail.com>
2026-03-18 01:04:26 +0000

    Boilerplate for 20260318
    https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/

---
+++ 202603/20260318.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/
+
+
+class Solution:
+    """3070. Count Submatrices with Top-Left Element and Sum Less Than k
+
+    You are given a **0-indexed** integer matrix `grid` and an integer `k`.
+
+    Return *the **number** of submatrices that contain the top-left element of the*
+    `grid`, *and have a sum less than or equal to* `k`."""
+
+    def count_submatrices(self, grid: list[list[int]], k: int) -> int: ...
+
+    countSubmatrices = count_submatrices
