72385816670caf2070a2e1fda80b215185b95cf0
MinHyeong Lee <usupool94@gmail.com>
2026-05-11 01:49:47 +0000

    Boilerplate for 20260511
    https://leetcode.com/problems/separate-the-digits-in-an-array/

---
+++ 202605/20260511.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/separate-the-digits-in-an-array/
+
+
+class Solution:
+    """2553. Separate the Digits in an Array
+
+    Given an array of positive integers `nums`, return *an array* `answer` *that
+    consists of the digits of each integer in* `nums` *after separating them in **the
+    same order** they appear in* `nums`.
+
+    To separate the digits of an integer is to get all the digits it has in the same
+    order.
+
+    * For example, for the integer `10921`, the separation of its digits is
+    `[1,0,9,2,1]`."""
+
+    def separate_digits(self, nums: list[int]) -> list[int]: ...
+
+    separateDigits = separate_digits
--- 202605/README.md
+++ 202605/README.md
@@ -91,7 +91,9 @@
    </a>
   </td>
   <td class="mon">
-   11
+   <a href="20260511.py">
+    11
+   </a>
   </td>
   <td class="tue">
    12
