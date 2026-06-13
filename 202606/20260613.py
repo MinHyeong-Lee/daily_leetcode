2379afb1f95fa0f2dfdb54bff6e28d3f9187d0c8
MinHyeong Lee <usupool94@gmail.com>
2026-06-13 02:00:29 +0000

    Boilerplate for 20260613
    https://leetcode.com/problems/weighted-word-mapping/

---
+++ 202606/20260613.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/weighted-word-mapping/
+
+
+class Solution:
+    """3838. Weighted Word Mapping
+
+    You are given an array of strings `words`, where each string represents a word
+    containing lowercase English letters.
+
+    You are also given an integer array `weights` of length 26, where `weights[i]`
+    represents the weight of the `ith` lowercase English letter.
+
+    The **weight** of a word is defined as the **sum** of the weights of its characters.
+
+    For each word, take its weight modulo 26 and map the result to a lowercase English
+    letter using reverse alphabetical order (`0 -> 'z', 1 -> 'y', ..., 25 -> 'a'`).
+
+    Return a string formed by concatenating the mapped characters for all words in
+    order."""
+
+    def map_word_weights(self, words: list[str], weights: list[int]) -> str: ...
+
+    mapWordWeights = map_word_weights
