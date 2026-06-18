79c5e4ad206423726cf8044ec80cb6586e2b02a7
MinHyeong Lee <usupool94@gmail.com>
2026-06-18 02:12:09 +0000

    Boilerplate for 20260618
    https://leetcode.com/problems/angle-between-hands-of-a-clock/

---
+++ 202606/20260618.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/angle-between-hands-of-a-clock/
+
+
+class Solution:
+    """1344. Angle Between Hands of a Clock
+
+    Given two numbers, `hour` and `minutes`, return *the smaller angle (in degrees)
+    formed between the* `hour` *and the* `minute` *hand*.
+
+    Answers within `10-5` of the actual value will be accepted as correct."""
+
+    def angle_clock(self, hour: int, minutes: int) -> float: ...
+
+    angleClock = angle_clock
