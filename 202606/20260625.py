17638185222b5e56bac98f9bf253850b2a5ae9f1
MinHyeong Lee <usupool94@gmail.com>
2026-06-25 01:57:54 +0000

    Boilerplate for 20260625
    https://leetcode.com/problems/count-subarrays-with-majority-element-i/

---
+++ 202606/20260625.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/count-subarrays-with-majority-element-i/
+
+
+class Solution:
+    """3737. Count Subarrays With Majority Element I
+
+    You are given an integer array `nums` and an integer `target`.
+
+    Return the number of **subarrays** of `nums` in which `target` is the **majority
+    element**.
+
+    The **majority element** of a subarray is the element that appears **strictly**
+    **more than half** of the times in that subarray."""
+
+    def count_majority_subarrays(self, nums: list[int], target: int) -> int: ...
+
+    countMajoritySubarrays = count_majority_subarrays
