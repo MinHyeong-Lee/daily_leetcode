3bcc6a500999bb8bbe38ac2f8b4be48376a0424c
MinHyeong Lee <usupool94@gmail.com>
2026-06-29 02:04:05 +0000

    Boilerplate for 20260629
    https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/

---
+++ 202606/20260629.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/
+
+
+class Solution:
+    """1967. Number of Strings That Appear as Substrings in Word
+
+    Given an array of strings `patterns` and a string `word`, return *the **number** of
+    strings in* `patterns` *that exist as a **substring** in* `word`.
+
+    A **substring** is a contiguous sequence of characters within a string."""
+
+    def num_of_strings(self, patterns: list[str], word: str) -> int: ...
+
+    numOfStrings = num_of_strings
