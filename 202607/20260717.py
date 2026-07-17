b68ba1f7014116a6e6661f1a3cd1875d35fa9dc4
MinHyeong Lee <usupool94@gmail.com>
2026-07-17 01:20:04 +0000

    Boilerplate for 20260717
    https://leetcode.com/problems/sorted-gcd-pair-queries/

---
+++ 202607/20260717.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/sorted-gcd-pair-queries/
+
+
+class Solution:
+    """3312. Sorted GCD Pair Queries
+
+    You are given an integer array `nums` of length `n` and an integer array `queries`.
+
+    Let `gcdPairs` denote an array obtained by calculating the GCD of all possible pairs
+    `(nums[i], nums[j])`, where `0 <= i < j < n`, and then sorting these values in
+    **ascending** order.
+
+    For each query `queries[i]`, you need to find the element at index `queries[i]` in
+    `gcdPairs`.
+
+    Return an integer array `answer`, where `answer[i]` is the value at
+    `gcdPairs[queries[i]]` for each query.
+
+    The term `gcd(a, b)` denotes the **greatest common divisor** of `a` and `b`."""
+
+    def gcd_values(self, nums: list[int], queries: list[int]) -> list[int]: ...
+
+    gcdValues = gcd_values
