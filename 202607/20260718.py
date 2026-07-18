ef5060a612f6588d05832e5d0d45e812479642da
MinHyeong Lee <usupool94@gmail.com>
2026-07-18 01:12:34 +0000

    Boilerplate for 20260718
    https://leetcode.com/problems/find-greatest-common-divisor-of-array/

---
+++ 202607/20260718.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+
+
+class Solution:
+    """1979. Find Greatest Common Divisor of Array
+
+    Given an integer array `nums`, return*the **greatest common divisor** of the
+    smallest number and largest number in* `nums`.
+
+    The **greatest common divisor** of two numbers is the largest positive integer that
+    evenly divides both numbers."""
+
+    def find_g_c_d(self, nums: list[int]) -> int: ...
+
+    findGCD = find_g_c_d
