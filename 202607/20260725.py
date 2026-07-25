359b513da190275c1179ff55247852c23661bcc3
MinHyeong Lee <usupool94@gmail.com>
2026-07-25 01:20:32 +0000

    Boilerplate for 20260725
    https://leetcode.com/problems/maximum-product-of-two-digits/

---
+++ 202607/20260725.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/maximum-product-of-two-digits/
+
+
+class Solution:
+    """3536. Maximum Product of Two Digits
+
+    You are given a positive integer `n`.
+
+    Return the **maximum** product of any two digits in `n`.
+
+    **Note:** You may use the **same** digit twice if it appears more than once in `n`.
+    """
+
+    def max_product(self, n: int) -> int: ...
+
+    maxProduct = max_product
