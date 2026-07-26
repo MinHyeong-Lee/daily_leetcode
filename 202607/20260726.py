cf73bcedc3dc442964fd785f69385003ef031544
MinHyeong Lee <usupool94@gmail.com>
2026-07-26 01:23:18 +0000

    Boilerplate for 20260726
    https://leetcode.com/problems/maximum-product-of-three-numbers/

---
+++ 202607/20260726.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/maximum-product-of-three-numbers/
+
+
+class Solution:
+    """628. Maximum Product of Three Numbers
+
+    Given an integer array `nums`, *find three numbers whose product is maximum and
+    return the maximum product*."""
+
+    def maximum_product(self, nums: list[int]) -> int: ...
+
+    maximumProduct = maximum_product
