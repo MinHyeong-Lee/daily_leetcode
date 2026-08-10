6052405445afd863628eb45abdd50d18a04ec1f0
MinHyeong Lee <usupool94@gmail.com>
2026-08-10 00:43:26 +0000

    Boilerplate for 20260810
    https://leetcode.com/problems/stone-game-iv/

---
+++ 202608/20260810.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/stone-game-iv/
+
+
+class Solution:
+    """1510. Stone Game IV
+
+    Alice and Bob take turns playing a game, with Alice starting first.
+
+    Initially, there are `n` stones in a pile. On each player's turn, that player makes
+    a *move* consisting of removing **any** non-zero **square number** of stones in the
+    pile.
+
+    Also, if a player cannot make a move, he/she loses the game.
+
+    Given a positive integer `n`, return `true` if and only if Alice wins the game
+    otherwise return `false`, assuming both players play optimally."""
+
+    def winner_square_game(self, n: int) -> bool: ...
+
+    winnerSquareGame = winner_square_game
