b2cca1a4aea31028fdf25b3fed1bd428f3a79a1d
MinHyeong Lee <usupool94@gmail.com>
2026-08-11 00:43:10 +0000

    Boilerplate for 20260811
    https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/

---
+++ 202608/20260811.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/
+
+
+class Solution:
+    """2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
+
+    You are given a **0-indexed** array of integers `nums`.
+
+    A prefix `nums[0..i]` is **sequential** if, for all `1 <= j <= i`, `nums[j] = nums[j
+    - 1] + 1`. In particular, the prefix consisting only of `nums[0]` is **sequential**.
+
+    Return *the **smallest** integer* `x` *missing from* `nums` *such that* `x` *is
+    greater than or equal to the sum of the **longest** sequential prefix.*"""
+
+    def missing_integer(self, nums: list[int]) -> int: ...
+
+    missingInteger = missing_integer
