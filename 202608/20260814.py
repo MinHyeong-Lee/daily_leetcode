baa9a227d098bf2ba92dc00fe8dd577ac2a8b75b
MinHyeong Lee <usupool94@gmail.com>
2026-08-14 00:51:25 +0000

    Boilerplate for 20260814
    https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/

---
+++ 202608/20260814.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/maximum-length-substring-with-two-occurrences/
+
+
+class Solution:
+    """3090. Maximum Length Substring With Two Occurrences
+
+    Given a string `s`, return the **maximum** length of a substring such that it
+    contains *at most two occurrences* of each character."""
+
+    def maximum_length_substring(self, s: str) -> int: ...
+
+    maximumLengthSubstring = maximum_length_substring
