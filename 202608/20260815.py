a421eaf29377ae112d38751d32cb31e890b89551
MinHyeong Lee <usupool94@gmail.com>
2026-08-15 00:28:29 +0000

    Boilerplate for 20260815
    https://leetcode.com/problems/longest-subsequence-with-non-zero-bitwise-xor/

---
+++ 202608/20260815.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/longest-subsequence-with-non-zero-bitwise-xor/
+
+
+class Solution:
+    """3702. Longest Subsequence With Non-Zero Bitwise XOR
+
+    You are given an integer array `nums`.
+
+    Return the length of the **longest subsequence** in `nums` whose bitwise **XOR** is
+    **non-zero**. If no such **subsequence** exists, return 0."""
+
+    def longest_subsequence(self, nums: list[int]) -> int: ...
+
+    longestSubsequence = longest_subsequence
