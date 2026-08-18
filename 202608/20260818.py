271dfe195082026df4f67ad57c50dee016f8eee3
MinHyeong Lee <usupool94@gmail.com>
2026-08-18 00:27:31 +0000

    Boilerplate for 20260818
    https://leetcode.com/problems/find-the-largest-almost-missing-integer/

---
+++ 202608/20260818.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/find-the-largest-almost-missing-integer/
+
+
+class Solution:
+    """3471. Find the Largest Almost Missing Integer
+
+    You are given an integer array `nums` and an integer `k`.
+
+    An integer `x` is **almost missing** from `nums` if `x` appears in *exactly* one
+    subarray of size `k` within `nums`.
+
+    Return the **largest** **almost missing** integer from `nums`. If no such integer
+    exists, return `-1`.
+
+    A **subarray** is a contiguous sequence of elements within an array."""
+
+    def largest_integer(self, nums: list[int], k: int) -> int: ...
+
+    largestInteger = largest_integer
