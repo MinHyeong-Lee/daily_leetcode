01ecfb1a867ab440031487b96cacd61975a5dd95
MinHyeong Lee <usupool94@gmail.com>
2026-08-21 00:30:30 +0000

    Boilerplate for 20260821
    https://leetcode.com/problems/kth-smallest-amount-with-single-denomination-combination/

---
+++ 202608/20260821.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/kth-smallest-amount-with-single-denomination-combination/
+
+
+class Solution:
+    """3116. Kth Smallest Amount With Single Denomination Combination
+
+    You are given an integer array `coins` representing coins of different denominations
+    and an integer `k`.
+
+    You have an infinite number of coins of each denomination. However, you are **not
+    allowed** to combine coins of different denominations.
+
+    Return the `kth` **smallest** amount that can be made using these coins."""
+
+    def find_kth_smallest(self, coins: list[int], k: int) -> int: ...
+
+    findKthSmallest = find_kth_smallest
