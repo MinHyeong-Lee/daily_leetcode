e832c134d8d77a3ea84bf36aec56a26dc5251fff
MinHyeong Lee <usupool94@gmail.com>
2026-08-25 00:28:58 +0000

    Boilerplate for 20260825
    https://leetcode.com/problems/smallest-missing-multiple-of-k/

---
+++ 202608/20260825.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/smallest-missing-multiple-of-k/
+
+
+class Solution:
+    """3718. Smallest Missing Multiple of K
+
+    Given an integer array `nums` and an integer `k`, return the **smallest positive
+    multiple** of `k` that is **missing** from `nums`.
+
+    A **multiple** of `k` is any positive integer divisible by `k`."""
+
+    def missing_multiple(self, nums: list[int], k: int) -> int: ...
+
+    missingMultiple = missing_multiple
