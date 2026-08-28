90db33974ed9396cf82ff058b0c9d64f7d0d7783
MinHyeong Lee <usupool94@gmail.com>
2026-08-28 07:16:03 +0000

    Boilerplate for 20260828
    https://leetcode.com/problems/lexicographically-smallest-palindromic-permutation-greater-than-target/

---
+++ 202608/20260828.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/lexicographically-smallest-palindromic-permutation-greater-than-target/
+
+
+class Solution:
+    """3734. Lexicographically Smallest Palindromic Permutation Greater Than Target
+
+    You are given two strings `s` and `target`, each of length `n`, consisting of
+    lowercase English letters.
+
+    Return the **lexicographically smallest string** that is **both** a **palindromic
+    permutation** of `s` and **strictly** greater than `target`. If no such permutation
+    exists, return an empty string."""
+
+    def lex_palindromic_permutation(self, s: str, target: str) -> str: ...
+
+    lexPalindromicPermutation = lex_palindromic_permutation
