4986855498c3fd54f5e15107d1258fa7cd89bcf3
MinHyeong Lee <usupool94@gmail.com>
2026-09-06 01:30:00 +0000

    Boilerplate for 20260906
    https://leetcode.com/problems/distinct-subsequences/

---
+++ 202609/20260906.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/distinct-subsequences/
+
+
+class Solution:
+    """115. Distinct Subsequences
+
+    Given two strings s and t, return *the number of distinct* ***subsequences*** *of* s
+    *which equals* t.
+
+    The test cases are generated so that the answer fits on a 32-bit signed integer."""
+
+    def num_distinct(self, s: str, t: str) -> int: ...
+
+    numDistinct = num_distinct
