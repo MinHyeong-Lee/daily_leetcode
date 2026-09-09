c99e601c0581c753f5f8f91beab3206555653050
MinHyeong Lee <usupool94@gmail.com>
2026-09-09 01:45:26 +0000

    Boilerplate for 20260909
    https://leetcode.com/problems/count-commas-in-range-ii/

---
+++ 202609/20260909.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/count-commas-in-range-ii/
+
+
+class Solution:
+    """3871. Count Commas in Range II
+
+    You are given an integer `n`.
+
+    Return the **total** number of commas used when writing all integers from `[1, n]`
+    (inclusive) in **standard** number formatting.
+
+    In **standard** formatting:
+
+    * A comma is inserted after **every three** digits from the right.
+
+    * Numbers with **fewer** than 4 digits contain no commas."""
+
+    def count_commas(self, n: int) -> int: ...
+
+    countCommas = count_commas
