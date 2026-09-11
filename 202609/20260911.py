c684aecb828c03be855fb75567256ad8e1bac1d6
MinHyeong Lee <usupool94@gmail.com>
2026-09-11 01:39:36 +0000

    Boilerplate for 20260911
    https://leetcode.com/problems/unique-3-digit-even-numbers/

---
+++ 202609/20260911.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/unique-3-digit-even-numbers/
+
+
+class Solution:
+    """3483. Unique 3-Digit Even Numbers
+
+    You are given an array of digits called `digits`. Your task is to determine the
+    number of **distinct** three-digit even numbers that can be formed using these
+    digits.
+
+    **Note**: Each *copy* of a digit can only be used **once per number**, and there may
+    **not** be leading zeros."""
+
+    def total_numbers(self, digits: list[int]) -> int: ...
+
+    totalNumbers = total_numbers
