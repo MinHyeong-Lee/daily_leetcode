64e8667eb782bb2e9721228d050c98307ce2e6f1
MinHyeong Lee <usupool94@gmail.com>
2026-09-14 01:54:31 +0000

    Boilerplate for 20260914
    https://leetcode.com/problems/rectangle-overlap/

---
+++ 202609/20260914.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/rectangle-overlap/
+
+
+class Solution:
+    """836. Rectangle Overlap
+
+    An axis-aligned rectangle is represented as a list `[x1, y1, x2, y2]`, where `(x1,
+    y1)` is the coordinate of its bottom-left corner, and `(x2, y2)` is the coordinate
+    of its top-right corner. Its top and bottom edges are parallel to the X-axis, and
+    its left and right edges are parallel to the Y-axis.
+
+    Two rectangles overlap if the area of their intersection is **positive**. To be
+    clear, two rectangles that only touch at the corner or edges do not overlap.
+
+    Given two axis-aligned rectangles `rec1` and `rec2`, return `true` *if they overlap,
+    otherwise return* `false`."""
+
+    def is_rectangle_overlap(self, rec1: list[int], rec2: list[int]) -> bool: ...
+
+    isRectangleOverlap = is_rectangle_overlap
