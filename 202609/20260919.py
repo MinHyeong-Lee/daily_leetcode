6768d746210e57c97569533302233ccb2f072f92
MinHyeong Lee <usupool94@gmail.com>
2026-09-19 01:51:10 +0000

    Boilerplate for 20260919
    https://leetcode.com/problems/circle-and-rectangle-overlapping/

---
+++ 202609/20260919.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/circle-and-rectangle-overlapping/
+
+
+class Solution:
+    """1401. Circle and Rectangle Overlapping
+
+    You are given a circle represented as `(radius, x_center, y_center)` and an axis-
+    aligned rectangle represented as `(x1, y1, x2, y2)`, where `(x1, y1)` are the
+    coordinates of the bottom-left corner, and `(x2, y2)` are the coordinates of the
+    top-right corner of the rectangle.
+
+    Return `true` *if the circle and rectangle are overlapped otherwise return* `false`.
+    In other words, check if there is **any** point `(xi, yi)` that belongs to the
+    circle and the rectangle at the same time."""
+
+    def check_overlap(
+        self,
+        radius: int,
+        x_center: int,
+        y_center: int,
+        x1: int,
+        y1: int,
+        x2: int,
+        y2: int,
+    ) -> bool: ...
+
+    checkOverlap = check_overlap
