4e691701a7a62d8893dac8a258a134171119f055
MinHyeong Lee <usupool94@gmail.com>
2026-09-23 02:00:03 +0000

    Boilerplate for 20260923
    https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/

---
+++ 202609/20260923.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
+
+
+class Solution:
+    """1658. Minimum Operations to Reduce X to Zero
+
+    You are given an integer array `nums` and an integer `x`. In one operation, you can
+    either remove the leftmost or the rightmost element from the array `nums` and
+    subtract its value from `x`. Note that this **modifies** the array for future
+    operations.
+
+    Return *the **minimum number** of operations to reduce* `x` *to **exactly*** `0` *if
+    it is possible**, otherwise, return* `-1`."""
+
+    def min_operations(self, nums: list[int], x: int) -> int: ...
+
+    minOperations = min_operations
