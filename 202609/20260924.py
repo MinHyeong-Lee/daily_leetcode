68f72b4ad594313589b4744c2c45f4ffde985d63
MinHyeong Lee <usupool94@gmail.com>
2026-09-24 01:49:03 +0000

    Boilerplate for 20260924
    https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/

---
+++ 202609/20260924.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/smallest-index-with-digit-sum-equal-to-index/
+
+
+class Solution:
+    """3550. Smallest Index With Digit Sum Equal to Index
+
+    You are given an integer array `nums`.
+
+    Return the **smallest** index `i` such that the sum of the digits of `nums[i]` is
+    equal to `i`.
+
+    If no such index exists, return `-1`."""
+
+    def smallest_index(self, nums: list[int]) -> int: ...
+
+    smallestIndex = smallest_index
